7864c406600e9cf3874c36cbeebfa8cd65e4500a
Eduardo Diez <deduar@gmail.com>
2021-08-24 15:37:19 +0200

    process DB async-await, clean code

--- controllers/process.js
+++ controllers/process.js
@@ -24,7 +24,7 @@ async function index(req, res) {
                 transmision: line[43],
                 year: line[82]
             }
-            
+
 
             const addVehicle = await models.Vehicle.create(vehicle);
 
@@ -40,7 +40,7 @@ async function index(req, res) {
                 var makeId = findMake[0].id;
             }
 
-            // Buscando Model
+            // Buscando Model (Pattern)
             const findModel = await models.Pattern.findAll({where: {description:vehicle.model}, group:['description']});
             if(findModel.length == 0){
                 var patter = {
@@ -53,7 +53,7 @@ async function index(req, res) {
                 var modelId = findModel[0].id;
             }
 
-            // Buscando Version
+            // Buscando Version (description)
             const findVersion = await models.Version.findAll({where: {description:vehicle.description}, group:['description']});
             if(findVersion.length == 0){
                 var version = {
@@ -65,8 +65,6 @@ async function index(req, res) {
             }else{
                 var versionId = findVersion[0].id;
             }
-            
-            console.log(versionId);
 
         }
         res.send('migrate succesfull');
@@ -76,126 +74,6 @@ async function index(req, res) {
 
 }
 
-async function splitMark(req, res) {
-    const allVehiclesMark = await models.Vehicle.findAll({ attributes: ['make'], group: ['make'] });
-    for (let index = 0; index < allVehiclesMark.length; index++) {
-        models.Make.findAll({ attributes: ['description'], where: { description: allVehiclesMark[index].make } }).then(result => {
-            if (result.length == 0) {
-                var newMark = {
-                    description: allVehiclesMark[index].make
-                };
-                models.Make.create(newMark).then(result => {
-                    if (result) {
-                        res.status(200);
-                    }
-                }).catch(error => {
-                    res.status(500).json({
-                        message: "Something went worng",
-                        error: error
-                    })
-                });
-            }
-        }).catch(error => {
-            res.status(500).json({
-                message: "Something went worng",
-                error: error
-            })
-        });
-    }
-
-    res.json({
-        message: "OK"
-    })
-}
-
-async function splitModel(req, res) {
-    const allVehiclesMark = await models.Make.findAll();
-    const allVehiclesModels = await models.Vehicle.findAll({ attributes: ['make', 'model'], group: ['model'], order: ['make'] });
-    for (let index = 0; index < allVehiclesModels.length; index++) {
-        models.Pattern.findAll({ where: { 'description': allVehiclesModels[index].model } }).then(result => {
-            if (result.length == 0) {
-                for (let index2 = 0; index2 < allVehiclesMark.length; index2++) {
-                    if (allVehiclesModels[index].make == allVehiclesMark[index2].description) {
-                        //allVehiclesModels[index].make = allVehiclesMark[index2].id;
-                        var newPattern = {
-                            makeId: allVehiclesMark[index2].id,
-                            description: allVehiclesModels[index].model
-                        }
-                        break;
-                    }
-                }
-                models.Pattern.create(newPattern).then(result => {
-                    if (result) {
-                        res.status(200);
-                    }
-                }).catch(error => {
-                    res.status(500).json({
-                        message: "Something went worng",
-                        error: error
-                    })
-                });
-            }
-        }).catch(error => {
-            res.status(500).json({
-                message: "Something went worng",
-                error: error
-            })
-        });
-    }
-
-    res.json({
-        message: "OK"
-    })
-}
-
-
-async function splitVersion(req, res) {
-    const allVehiclesModels = await models.Pattern.findAll();
-    const allVehiclesVersions = await models.Vehicle.findAll({ attributes: ['model', 'description'], order: ['model'] });
-
-    for (let index = 0; index < allVehiclesVersions.length; index++) {
-        models.Version.findAll({ where: { 'description': allVehiclesModels[index].description } }).then(result => {
-            if (result.length == 0) {
-                for (let index2 = 0; index2 < allVehiclesModels.length; index2++) {
-                    if (allVehiclesVersions[index].model == allVehiclesModels[index2].description) {
-                        var newVersion = {
-                            modelId: allVehiclesModels[index2].id,
-                            description: allVehiclesVersions[index].description
-                        }
-                        break;
-                    }
-                }
-                models.Version.create(newVersion).then(result => {
-                    if (result) {
-                        res.status(200);
-                    }
-                }).catch(error => {
-                    res.status(500).json({
-                        message: "Something went worng",
-                        error: error
-                    })
-                });
-            } else {
-                console.log('Nothing to doing');
-            }
-        }).catch(error => {
-            res.status(500).json({
-                message: "Something went worng",
-                error: error
-            })
-        });
-
-    }
-
-
-    res.json({
-        message: "OK"
-    })
-}
-
 module.exports = {
     index: index,
-    splitMark: splitMark,
-    splitModel: splitModel,
-    splitVersion: splitVersion
 }
--- routes/process.js
+++ routes/process.js
@@ -3,8 +3,5 @@ const router = Router();
 const fileProcessController= require('../controllers/process');
 
 router.get('/',fileProcessController.index);
-router.get('/splitMark',fileProcessController.splitMark);
-router.get('/splitModel',fileProcessController.splitModel);
-router.get('/splitVersion',fileProcessController.splitVersion);
 
 module.exports = router;
