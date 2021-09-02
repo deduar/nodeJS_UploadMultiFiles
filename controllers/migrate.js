702757bc19f72ce6de7fb905d75bd75f1970348f
deduar <deduar@gmail.com>
2021-09-02 05:49:37 -0400

    conn and select all post product and != admin

--- controllers/migrate.js
+++ controllers/migrate.js
@@ -1,7 +1,28 @@
 const models = require('../models');
-
+const mysql = require('mysql2');
+const dbconfig = require(__dirname + '/../config/db.config.json');
 
 function index(req, res) {
+
+    const con = mysql.createConnection({
+        host: dbconfig.HOST,
+        user: dbconfig.USER,
+        password: dbconfig.PASSWORD,
+        database: dbconfig.DB
+    });
+
+    con.connect(error => {
+        if (error) throw error;
+        console.log("Successfully connected to the database.");
+    });
+
+    con.query('SELECT * FROM optnc_posts WHERE `post_type`=\'product\' AND `post_author`!=1', (err, res) => {
+        if(err){
+            console.log("error: ", err);
+        }
+        console.log("posts: ",res.length);
+    });
+
     res.status(200).json({
         message: "OK migrate"
     });
