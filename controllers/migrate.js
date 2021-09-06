63e055f3f64503493834410eddd5b9825d30cb32
deduar <deduar@gmail.com>
2021-09-06 09:16:40 -0400

    migrate import data from WP post_meta

--- controllers/migrate.js
+++ controllers/migrate.js
@@ -30,19 +30,75 @@ async function index(req, res) {
                     if (resultpatternId.length > 0) {
                         conn.query('SELECT `meta_value` FROM `optnc_postmeta` WHERE `meta_key`=\'matricula\' AND `post_id`= ' + post.ID).then(register => {
                             if (register.length > 0) {
-                                conn.query('SELECT `meta_value` FROM `optnc_postmeta` WHERE `meta_key`=\'combustibe\' AND `post_id`= ' + post.ID).then(combustible => {
-                                    if (combustible.length > 0) {
+                                conn.query('SELECT meta_key,meta_value FROM `optnc_postmeta` WHERE `post_id`= ' + post.ID).then(meta_values => {
+                                    meta_values.forEach(meta_value => {
+                                        meta_value.forEach(element => {
+                                            switch (element.meta_key) {
+                                                case '_price':
+                                                    price = element.meta_value;
+                                                    break;
+                                                case 'km':
+                                                    km = element.meta_value
+                                                    break;
+                                                case 'ano_matriculacion':
+                                                    ano_matriculacion = element.meta_value
+                                                    break;
+                                                case 'potencia':
+                                                    potencia = element.meta_value
+                                                    break;
+                                                case 'acabado':
+                                                    acabado = element.meta_value
+                                                    break;
+                                                case 'matricula':
+                                                    matricula = element.meta_value
+                                                    break;
+                                                case 'tipo':
+                                                    tipo = element.meta_value
+                                                    break;
+                                                case 'combustible':
+                                                    combustible = element.meta_value
+                                                    break;
+                                                case 'caja_cambios':
+                                                    caja_cambios = element.meta_value
+                                                    break;
+                                                case 'puertas':
+                                                    puertas = element.meta_value
+                                                    break;
+                                                case 'plazas':
+                                                    plazas = element.meta_value
+                                                    break;
+                                                case 'color':
+                                                    color = element.meta_value
+                                                    break;
+                                                case 'provincia':
+                                                    provincia = element.meta_value
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        });
                                         newPost = {
                                             carId: 'WP_' + post.ID + '_' + post.post_author,
                                             make: resultMakeId[0].id,
                                             model: resultpatternId[0].id,
-                                            register: register[0][0].meta_value,
-                                            combustible: combustible[0]
+                                            price: price,
+                                            km: km,
+                                            ano_matriculacion: ano_matriculacion,
+                                            potencia: potencia,
+                                            acabado: acabado,
+                                            matricula: matricula,
+                                            tipo: tipo,
+                                            combustible: combustible,
+                                            caja_cambios: caja_cambios,
+                                            puertas: puertas,
+                                            plazas: plazas,
+                                            color: color,
+                                            provincia: provincia
                                         }
-                                        console.log(newPost);
-                                    }
+                                    });
+                                    console.log(newPost);
                                 }).catch(error => {
-                                    console.log(error);
+                                    console.log(error)
                                 });
                             }
                         }).catch(error => {
@@ -62,34 +118,6 @@ async function index(req, res) {
 
 }
 
-
-async function test(req, res) {
-    const conn = await mysql.createConnection({
-        host: dbconfig.HOST,
-        user: dbconfig.USER,
-        password: dbconfig.PASSWORD,
-        database: dbconfig.DB
-    });
-
-    await conn.connect(error => {
-        if (error) throw error;
-        console.log("Successfully connected to the database.");
-    });
-
-    const wp_posts = await conn.query('SELECT * FROM optnc_posts WHERE `post_type`=\'product\' AND `post_author`!=1');
-
-    console.log(wp_posts[0].length);
-
-    Promise.resolve('SELECT `meta_value` FROM `optnc_postmeta` WHERE `meta_key`=\'matricula\' AND `post_id`='+wp_posts[0][0].ID).then(function (value) {
-        console.log(conn.query(value)); // "Success"
-    }, function (value) {
-        // not called
-    });
-
-    res.status(200).json({ message: "OK" });
-}
-
 module.exports = {
-    index: index,
-    test: test
+    index: index
 }
--- routes/migrate.js
+++ routes/migrate.js
@@ -3,6 +3,5 @@ const router = Router();
 const migrateController= require('../controllers/migrate');
 
 router.get('/',migrateController.index);
-router.get('/test',migrateController.test);
 
 module.exports = router;
